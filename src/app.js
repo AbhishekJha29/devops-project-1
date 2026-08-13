df617ccd4862c953b8e7863882646bfe2fd877f8
AbhishekJha29 <abbishekjha2286@gmail.com>
2026-08-13 20:08:37 +0530

    Minor change

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.status(200).json({
     message: 'Hello from CI/CD Pipeline Demo App',
-    version: '1.0.0'
+    version: '1.0.1'
   });
 });
 
